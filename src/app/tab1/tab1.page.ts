6a321cfb1c324c5dc8abcf69d2ce1836084ec3c4
Jaime <aquileseeb@gmail.com>
2020-02-17 10:13:53 +0100

    c23

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IdeaService } from '../services/idea.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  // var database: firebase.database();
+  listaPartidos =  [];
 
+  constructor(private idea: IdeaService){}
 
-  constructor(){}
-
+  ngOnInit(){
+    this.idea.getPartidos().subscribe(res => {
+      this.listaPartidos = res;
+      console.log(res);
+    });
+  }
 
 }
